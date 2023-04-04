fb508e1004e3a2048e78a44491e5ec244b110c37
Bhjamin <benjamin@hancock1.com>
2023-04-04 15:55:23 -0600

    created Ben.jsx

---
+++ src/Ben.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Ben = () => {
+  return (
+    <div>
+        <p>This is Bens div</p>
+        <p>No one touch Bens div please (and thank you)</p>
+    </div>
+  )
+}
+
+export default Ben
